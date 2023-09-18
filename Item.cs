1bd5c95f1bf27e65560eef9f2a605016d20ec4f8
_codefix <104313695+c0def1x@users.noreply.github.com>
2023-09-18 16:13:42 +0500

    Add files via upload

---
+++ Item.cs
@@ -0,0 +1,20 @@
+﻿namespace Backpack
+{
+    public class Item
+    {
+        public Item (int weight, double price)
+        {
+            Weight = weight;
+            Price = price;
+        }
+
+        public int Weight { get; set; }
+        public double Price { get; set; }
+
+        public double Relationship
+        {
+            get { return Price / Weight; }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using Backpack;
+using System.Text;
+
+List<Item> items = new List<Item>() 
+{
+    new Item(2, 1000), 
+    new Item(3, 500), 
+    new Item(5, 1500), 
+    new Item(7, 700), 
+    new Item(1, 600), 
+    new Item(4, 1800), 
+    new Item(1, 300) 
+};
+
+double fullPrice = 0, backPackWeight = 15;
+StringBuilder sb = new StringBuilder();
+
+foreach (var item in items.OrderByDescending(r => r.Relationship))
+{
+    if (item.Weight <= backPackWeight)
+    {
+        fullPrice += item.Price;
+        backPackWeight -= item.Weight;
+        sb.AppendLine($"Цена: {item.Price} - Вес: {item.Weight}");
+    }
+}
+
+Console.WriteLine(sb);
+Console.WriteLine($"Общая стоимость: {fullPrice}");
